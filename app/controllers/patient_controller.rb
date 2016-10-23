820f1402a87c79e25afc27c55ac074be05a314e5
drmeph <kevin.dorfer@gmail.com>
2016-10-23 19:26:43 -0400

    Edited controller + added routes
    Added all the basic actions in the controller

--- app/controllers/patient_controller.rb
+++ app/controllers/patient_controller.rb
@@ -1,2 +1,50 @@
 class PatientController < ApplicationController
+  def list
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+    @patient = Patient.find(params[:id])
+  end
+
+  def update
+    @patient = Patient.find(params[:id])
+
+    if @patient.update_attributes(patient_param)
+      redirect_to :action => 'list'
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def delete
+    Patient.find(params[:id]).destroy
+    redirect_to :action => 'list'
+  end
+
+  def patient_params
+    params.require(:patients).permit(:firstname, :middlename, :lastname, :weight, :height, :mrn)
+  end
+
+  def patient_param
+    params.require(:patient).permit(:firstname, :middlename, :lastname, :weight, :height, :mrn)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,16 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'patient/list'
+  get 'patient/new'
+  post 'patient/create'
+  patch 'patient/update'
+  get 'patient/list'
+  get 'patient/show'
+  get 'patient/edit'
+  get 'patient/delete'
+  get 'patient/update'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
