78f98451386840be6b135b64721a7a6ce7195863
drmeph <kevin.dorfer@gmail.com>
2016-10-24 13:53:10 -0400

    Added unit tests

--- test/models/encounter_test.rb
+++ test/models/encounter_test.rb
@@ -4,4 +4,39 @@ class EncounterTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save encounter without visitnumber" do
+    encounter = Encounter.new
+    assert_not encounter.save, "Saved the encounter without a visitnumber"
+  end
+
+  test "should not save encounter without admittedat" do
+    encounter = Encounter.new
+    encounter.visitnumber = "VN0015"
+    assert_not encounter.save, "Saved the encounter without a admittedat"
+  end
+
+  test "should not save encounter with a non numerical room" do
+    encounter = Encounter.new
+    encounter.visitnumber = "VN0015"
+    encounter.admittedat = DateTime.now.to_date
+    encounter.room = "string"
+    assert_not encounter.save
+  end
+
+  test "should not save encounter with a non numerical bed" do
+    encounter = Encounter.new
+    encounter.visitnumber = "VN0015"
+    encounter.admittedat = DateTime.now.to_date
+    encounter.bed = "string"
+    assert_not encounter.save
+  end
+
+  test "should not save encounter with discharedat before admittedat" do
+    encounter = Encounter.new
+    encounter.visitnumber = "VN0015"
+    encounter.admittedat = DateTime.now.to_date
+    encounter.discharedat = DateTime.yesterday.to_date
+    assert_not encounter.save
+  end
 end
--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -4,4 +4,79 @@ class PatientTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save patient without firstname" do
+    patient = Patient.new
+    patient.middlename = "alex"
+    patient.mrn = "BVN012"
+    assert_not patient.save, "Saved the patient without a firstname"
+  end
+
+  test "should not save patient without middlename" do
+    patient = Patient.new
+    patient.firstname = "kevin"
+    patient.mrn = "BVN012"
+    assert_not patient.save, "Saved the patient without a middlename"
+  end
+
+  test "should not save patient without mrn" do
+    patient = Patient.new
+    patient.firstname = "kevin"
+    patient.middlename = "alex"
+    assert_not patient.save, "Saved the patient without a mrn"
+  end
+
+  test "should not save patient with firstname longer than 45 characters" do
+    patient = Patient.new
+    long_name = LONG_STRING
+    patient.firstname = long_name
+    patient.middlename = "alex"
+    patient.mrn = "BVN012"
+    assert_not patient.save, "Saved the patient with firstname too long"
+  end
+
+  test "should not save patient with middlename longer than 45 characters" do
+    patient = Patient.new
+    long_name = LONG_STRING
+    patient.middlename = long_name
+    patient.firstname = "kevin"
+    patient.mrn = "BVN012"
+    assert_not patient.save, "Saved the patient with middlename too long"
+  end
+
+  test "should not save patient with lastname longer than 45 characters" do
+    patient = Patient.new
+    long_name = LONG_STRING
+    patient.firstname = "kevin"
+    patient.middlename = "alex"
+    patient.lastname = LONG_STRING
+    patient.mrn = "BVN012"
+    assert_not patient.save, "Saved the patient with lastname too long"
+  end
+
+  test "should not save patient with lastname longer than 6 characters" do
+    patient = Patient.new
+    patient.firstname = "kevin"
+    patient.middlename = "alex"
+    patient.mrn = "BVN012 too long"
+    assert_not patient.save, "Saved the patient with mrn too long"
+  end
+
+  test "should not save patient with a non numerical weight" do
+    patient = Patient.new
+    patient.firstname = "kevin"
+    patient.middlename = "alex"
+    patient.mrn = "BVN012 too long"
+    patient.weight = "string"
+    assert_not patient.save, "Saved the patient with non numerical weight"
+  end
+
+  test "should not save patient with a non numerical height" do
+    patient = Patient.new
+    patient.firstname = "kevin"
+    patient.middlename = "alex"
+    patient.mrn = "BVN012 too long"
+    patient.height = "string"
+    assert_not patient.save, "Saved the patient with non numerical height"
+  end
 end
