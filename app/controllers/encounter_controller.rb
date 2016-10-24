738cec99dda7419c8e9110e1c38afbc249bfa791
drmeph <kevin.dorfer@gmail.com>
2016-10-24 02:18:59 -0400

    Added encounter controller

---
+++ app/controllers/encounter_controller.rb
@@ -0,0 +1,44 @@
+class EncounterController < ApplicationController
+  layout 'standard'
+
+  def show
+    @encounter = Encounter.find(params[:id])
+  end
+
+  def new
+    @encounter = Encounter.new
+    @encounter.patient_id = params[:patient_id]
+  end
+
+  def edit
+    @encounter = Encounter.find(params[:id])
+  end
+
+  def create
+    @encounter = Encounter.new(encounter_params)
+
+    if @encounter.save
+      redirect_to :controller => 'patient', :action => 'show', :id => @encounter.patient_id
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @encounter = Encounter.find(params[:id])
+
+    if @encounter.update_attributes(encounter_params)
+      redirect_to :controller => 'patient', :action => 'show', :id => @encounter.patient_id
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def delete
+
+  end
+
+  def encounter_params
+    params.require(:encounter).permit(:visitnumber, :admittedat, :discharedat, :location, :room, :bed, :patient_id)
+  end
+end
