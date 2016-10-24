7e1db4d829328e32a08d645572bada1e4943ac31
drmeph <kevin.dorfer@gmail.com>
2016-10-24 02:20:46 -0400

    Updated models with validation

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,3 +1,13 @@
 class Encounter < ActiveRecord::Base
   belongs_to :patient
+  validates :visitnumber, :admittedat, presence: true
+  validates :room, :bed, numericality: true
+  validate :discharge_date_is_possible?
+
+  def discharge_date_is_possible?
+    return if[:discharedat.blank?, :admittedat.blank?].any?
+    if :discharedat > :admittedat
+      errors.add(:discharedat, 'must me after admission date')
+    end
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,5 @@
 class Patient < ActiveRecord::Base
   has_many :encounters
-  validates :firstname, :middlename, :mrn, presence : true
+  validates :firstname, :middlename, :mrn, presence: true
+  validates :weight, :height, numericality: true
 end
