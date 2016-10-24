b03a7802261c788a8bdef2918691b9c35214c6c1
drmeph <kevin.dorfer@gmail.com>
2016-10-24 02:20:24 -0400

    Added migration script to add patient_id column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023223956) do
+ActiveRecord::Schema.define(version: 20161024004527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20161023223956) do
     t.string   "location"
     t.integer  "room"
     t.integer  "bed"
+    t.integer  "patient_id"
   end
 
   create_table "patients", force: :cascade do |t|
